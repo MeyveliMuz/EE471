3ab7280181696b05f64090378342d3b1ad050894
Huseyin Cengiz <hscengiz96@gmail.com>
2026-03-05 14:48:32 +0300

    correction

--- conform.py
+++ conform.py
@@ -50,11 +50,3 @@ def pleaseConform(caps):
             
 pleaseConform(caps)
 ##pleaseConform(cap2)
-def pleaseConformOnepass(caps):
-    caps = caps + [caps[0]]
-    for i in range(1, len(caps)):
-        if caps[i] != caps[i-1]:
-            if caps[i] != caps[0]:
-                print('People in positions', i, end='')
-            else:
-                print(' through', i-1, 'flip your caps!')
